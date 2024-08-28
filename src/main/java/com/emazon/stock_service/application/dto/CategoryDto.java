9683b1a5366f9135e246e43f275de2aa229a630e
Juan Camilo Ortiz <juan.ortiz.gonzalez@correounivalle.edu.co>
2024-08-28 09:12:05 -0500

    Swagger documentation generated for HU1 and H2

--- src/main/java/com/emazon/stock_service/application/dto/CategoryDto.java
+++ src/main/java/com/emazon/stock_service/application/dto/CategoryDto.java
@@ -1,4 +1,5 @@
 package com.emazon.stock_service.application.dto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -8,12 +9,15 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Category Data Transfer Object")
 public class CategoryDto {
 
+    @Schema(description = "Category name. Must be unique and cannot exceed 50 characters.")
     @NotEmpty(message = "Category name cannot be empty")
     @Size(max = 50, message = "Category name's length cannot be longer than 50")
     private String name;
 
+    @Schema(description = "Category description. Cannot exceed 50 characters.")
     @NotEmpty(message = "Category description cannot be empty")
     @Size(max = 90, message = "Category description's length cannot be longer than 90")
     private String description;
--- src/main/java/com/emazon/stock_service/infrastructure/controller/CategoryController.java
+++ src/main/java/com/emazon/stock_service/infrastructure/controller/CategoryController.java
@@ -2,6 +2,12 @@ package com.emazon.stock_service.infrastructure.controller;
 
 import com.emazon.stock_service.application.dto.CategoryDto;
 import com.emazon.stock_service.application.handler.ICategoryHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +21,24 @@ import java.util.List;
 public class CategoryController {
     private final ICategoryHandler categoryHandler;
 
+    @Operation(summary = "Creates a new category", description = "Creates a new category from the data entered in the body")
+    @ApiResponse(responseCode = "201", description = "Category added successfully")
+    @ApiResponse(responseCode = "400", description = "Bad request")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Category object to be created", required = true, content = @Content(schema = @Schema(implementation = CategoryDto.class)))
+    @Tag(name = "Category operations")
     @PostMapping
     public ResponseEntity<String> createCategory(@RequestBody CategoryDto categoryDto) {
         categoryHandler.saveCategory(categoryDto);
         return ResponseEntity.status(HttpStatus.CREATED).body("The " + categoryDto.getName() + " category has been created");
     }
 
+    @Operation(summary = "Gets all categories", description = "Gets all the categories and displays them paginated and sorted according to the parameter")
+    @ApiResponse(responseCode = "200", description = "All categories have been obtained")
+    @ApiResponse(responseCode = "400", description = "Bad request")
+    @Parameter(name = "order", description = "Order of elements: 'asc' or 'desc", required = true)
+    @Tag(name = "Category operations")
     @GetMapping
     public ResponseEntity<List<CategoryDto>> getCategories(@RequestParam String order) {
-        return ResponseEntity.status(HttpStatus.FOUND).body(categoryHandler.getAllCategories(order).getContent());
+        return ResponseEntity.status(HttpStatus.OK).body(categoryHandler.getAllCategories(order).getContent());
     }
 }
