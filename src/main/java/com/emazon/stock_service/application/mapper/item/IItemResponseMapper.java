437df01ca443246090b8888a9098d09d651f4f6e
Juan Camilo Ortiz <juan.ortiz.gonzalez@correounivalle.edu.co>
2024-09-08 14:36:26 -0500

    Refactor code with Sonalint suggestions

--- src/main/java/com/emazon/stock_service/application/mapper/item/IItemResponseMapper.java
+++ src/main/java/com/emazon/stock_service/application/mapper/item/IItemResponseMapper.java
@@ -1,7 +1,5 @@
 package com.emazon.stock_service.application.mapper.item;
 
-import com.emazon.stock_service.application.dto.item.ItemResponse;
-import com.emazon.stock_service.domain.model.Item;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
