cb61d3f86d4bfc7ce1a53e298a6f051db091032b
Juan Camilo Ortiz <juan.ortiz.gonzalez@correounivalle.edu.co>
2024-09-08 16:41:47 -0500

    Implement item jpa adapter

--- src/main/java/com/emazon/stock_service/infrastructure/adapter/ItemJpaAdapter.java
+++ src/main/java/com/emazon/stock_service/infrastructure/adapter/ItemJpaAdapter.java
@@ -4,10 +4,17 @@ import com.emazon.stock_service.domain.model.CustomPagination;
 import com.emazon.stock_service.domain.model.Item;
 import com.emazon.stock_service.domain.spi.IItemPersistentPort;
 import com.emazon.stock_service.domain.utils.PaginationInfo;
+import com.emazon.stock_service.domain.utils.SortArgument;
+import com.emazon.stock_service.infrastructure.entity.ItemEntity;
 import com.emazon.stock_service.infrastructure.mapper.IItemEntityMapper;
 import com.emazon.stock_service.infrastructure.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -22,6 +29,32 @@ public class ItemJpaAdapter implements IItemPersistentPort {
 
     @Override
     public CustomPagination<Item> getAllItemsWithPagination(PaginationInfo paginationInfo) {
-        return null;
+        PageRequest pageRequest = PageRequest.of(paginationInfo.getPageNumber(), paginationInfo.getPageSize());
+        Page<ItemEntity> itemPage = null;
+
+        if (Objects.equals(SortArgument.ITEM_NAME.getField(), paginationInfo.getSortBy())) {
+            itemPage = paginationInfo.isAsc()
+                    ? itemRepository.findAllOrderByItemNameAsc(pageRequest)
+                    : itemRepository.findAllOrderByItemNameDesc(pageRequest);
+        } else if (Objects.equals(SortArgument.BRAND_NAME.getField(), paginationInfo.getSortBy())) {
+            itemPage = paginationInfo.isAsc()
+                    ? itemRepository.findAllOrderByNameAsc(pageRequest)
+                    : itemRepository.findAllOrderByNameDesc(pageRequest);
+        } else if (Objects.equals(SortArgument.NUMBER_OF_CATEGORIES.getField(), paginationInfo.getSortBy())) {
+            itemPage = paginationInfo.isAsc()
+                    ? itemRepository.findAllOrderByNumberOfCategoryIdListAsc(pageRequest)
+                    : itemRepository.findAllOrderByNumberOfCategoryIdListDesc(pageRequest);
+        }
+
+        assert itemPage != null;
+        List<Item> items = itemEntityMapper.toItemList(itemPage.getContent());
+
+        return new CustomPagination<>(
+                items,
+                itemPage.getTotalElements(),
+                itemPage.getTotalPages(),
+                paginationInfo.getPageNumber(),
+                paginationInfo.isAsc()
+        );
     }
 }
--- src/main/java/com/emazon/stock_service/infrastructure/mapper/IItemEntityMapper.java
+++ src/main/java/com/emazon/stock_service/infrastructure/mapper/IItemEntityMapper.java
@@ -7,6 +7,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring", uses = {
         ICategoryEntityMapper.class,
         IBrandEntityMapper.class,
@@ -15,6 +17,9 @@ public interface IItemEntityMapper {
     @Mapping(source = "brandId", target = "brandId", qualifiedByName = "idToBrand")
     @Mapping(source = "categoryIdList", target = "categoryIdList", qualifiedByName = "idToCategory")
     ItemEntity toItemEntity(Item item);
+
+    @Mapping(source = "categoryIdList", target = "categoryIdList")
+    List<Item> toItemList(List<ItemEntity> itemEntityList);
 
     @Named("idToCategory")
     default CategoryEntity idToCategory(Long categoryId) {
