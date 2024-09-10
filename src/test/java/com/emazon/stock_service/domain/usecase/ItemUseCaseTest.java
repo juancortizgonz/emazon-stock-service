4ba38a93cd33e3fa4edce94abbd84a40cb9ba11c
Juan Camilo Ortiz <juan.ortiz.gonzalez@correounivalle.edu.co>
2024-09-10 08:30:58 -0500

    Add unit tests

--- src/test/java/com/emazon/stock_service/domain/usecase/ItemUseCaseTest.java
+++ src/test/java/com/emazon/stock_service/domain/usecase/ItemUseCaseTest.java
@@ -1,22 +1,66 @@
 package com.emazon.stock_service.domain.usecase;
 
+import com.emazon.stock_service.domain.model.Brand;
+import com.emazon.stock_service.domain.model.Category;
 import com.emazon.stock_service.domain.model.Item;
 import com.emazon.stock_service.domain.spi.IItemPersistentPort;
+import com.emazon.stock_service.infrastructure.repository.BrandRepository;
+import com.emazon.stock_service.infrastructure.repository.CategoryRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class ItemUseCaseTest {
-    private final IItemPersistentPort itemPersistentPortMock = mock(IItemPersistentPort.class, "itemPersistentPort");
+    @Mock
+    private BrandRepository brandRepository;
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @Mock
+    private IItemPersistentPort itemPersistentPort;
+
+    @InjectMocks
+    private ItemUseCase itemUseCase;
 
     @Test
-    void saveItem() {
-        Item item = mock(Item.class);
-        doNothing().when(itemPersistentPortMock).saveItem(item);
-        ItemUseCase useCase = new ItemUseCase(itemPersistentPortMock);
-        useCase.saveItem(item);
+    void shouldCreateItemWhenBrandAndCategoriesExist() {
+        Brand brand = new Brand(1L, "Brand name", "Brand description");
+        Category category = new Category(1L, "Category name", "Category description");
+        List<Long> categories = new ArrayList<>(List.of(1L));
+        Item item = new Item(1L, "Item name", "Item description", 1, 2000.0, 1L, categories);
 
-        assertAll("Result", () -> verify(itemPersistentPortMock).saveItem(item));
+        itemUseCase.saveItem(item);
+        verify(itemPersistentPort, times(1)).saveItem(item);
+    }
+
+    @Test
+    void shouldNotSaveItemWhenBrandDoesNotExist() {
+        Category category = new Category(1L, "Category name", "Category description");
+        List<Long> categories = new ArrayList<>(List.of(1L));
+        Item item = new Item(1L, "Item name", "Item description", 1, 2000.0, 2L, categories);
+
+        verify(itemPersistentPort, never()).saveItem(item);
+    }
+
+    @Test
+    void shouldNotSaveItemWhenCategoryDoesNotExist() {
+        Brand brand = new Brand(1L, "Brand name", "Brand description");
+        List<Long> categories = new ArrayList<>(List.of(1L));
+        Item item = new Item(1L, "Item name", "Item description", 1, 2000.0, 1L, categories);
+
+        verify(itemPersistentPort, never()).saveItem(item);
+    }
+
+    @Test
+    void getAllItemsWithPagination() {
     }
 }
